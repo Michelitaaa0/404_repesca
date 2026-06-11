5bef9a54b205b84db63bfed6ce72abbe643deb27
Michelitaaa0 <michelasantilli.13@gmail.com>
2026-06-11 16:44:46 +0200

    Add function to create and append stars to the 404 error page

---
+++ script.js
@@ -0,0 +1,17 @@
+const contenedor = document.getElementById("estrellas");
+
+function crearEstrellas() {
+  for (let i = 0; i < 120; i++) {
+    const estrella = document.createElement("div");
+    estrella.classList.add("estrella");
+
+    const tamanio = Math.random() * 2 + 1;
+    estrella.style.width = tamanio + "px";
+    estrella.style.height = tamanio + "px";
+    estrella.style.top = Math.random() * 100 + "%";
+    estrella.style.left = Math.random() * 100 + "%";
+    estrella.style.animationDelay = Math.random() * 3 + "s";
+
+    contenedor.appendChild(estrella);
+  }
+}
